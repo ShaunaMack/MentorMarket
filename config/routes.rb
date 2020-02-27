6a9be1117aae8fae1782692e83546bc47c3b9f2d
Shauna Mack <shaunakatemack@gmail.com>
2020-02-27 22:57:09 +1000

    simple rails messaging working no styling

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'conversations#index'
+
+  resources :users, only: [:index]
+  resources :personal_messages, only: [:new, :create]
+  resources :conversations, only: [:index, :show]
+
+end
