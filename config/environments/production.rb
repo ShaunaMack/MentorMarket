757b4dee69db80dd02c42dc21b4709368978dc54
Shauna Mack <shaunakatemack@gmail.com>
2020-03-09 20:34:18 +1000

    production mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandbox177057f4369b4920aa6d10372542139c.mailgun.org",
+    :user_name => Rails.application.credentials.dig(:mailgun, :user_name),
+    :password => Rails.application.credentials.dig(:mailgun, :password)
+  }
+  config.action_mailer.default_url_options = { host: 'mentor-market.herokuapp.com' }
+
 end
