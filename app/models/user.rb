b78caabf6319b0000b07d53deb1866f831558d68
Shauna Mack <shaunakatemack@gmail.com>
2020-03-09 19:36:42 +1000

    users verify email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandbox177057f4369b4920aa6d10372542139c.mailgun.org",
+    :user_name => Rails.application.credentials.dig(:mailgun, :user_name),
+    :password => Rails.application.credentials.dig(:mailgun, :password)
+  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
